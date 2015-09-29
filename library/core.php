b7c0db1294fad2c00d6ad7f97ef56cdc63ad706e
HUSzanaI <HUSzanaI@HUDEB606345.ni.corp.natinst.com>
2015-09-29 16:22:09 +0200

    Létrehoztam a Core osztályt

---
+++ library/core.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * ID: $Id
+ * File: core.php
+ * Namespace: library
+ * 
+ * Description of core
+ * 
+ * 
+ *  Version     Date            Author               Changelog   
+ *   1.0.0      2015.09.29.     HUSzanaI              Created
+ * 
+ */
+
+namespace library;
+
+class core {
+    //put your code here
+}
+
+?>
