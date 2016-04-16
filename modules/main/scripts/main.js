48f834dfb36a28eedaeb3283f39f66357a2f7041
Pisti <szana8@gmail.com>
2016-04-16 14:44:50 +0200

    -

--- modules/main/scripts/main.js
+++ modules/main/scripts/main.js
@@ -7,10 +7,19 @@
 $(document).ready(function() {
     setGadgetDefault();
     setSortableGrid();
+    setGadgetColor();
 });
 
 function setGadgetDefault() {
     $('.app-gadget').css('cursor', 'move');
+    $('.app-gadget').hover(function(event) {
+        $(this).find(".gadget-options").show();
+    });
+
+    $('.app-gadget').mouseleave(function(event) {
+        $(this).find(".gadget-options").hide();
+        $("button.dropdown-toggle").dropdown("toggle");
+    });
 
     $('.gadget-minimize').click(function() {
         $(this).parents(':eq(5)').next('.panel-body').hide('slideUp');
@@ -22,16 +31,33 @@ function setGadgetDefault() {
 }
 
 function setSortableGrid() {
-    var fromClass = '';
-    var toClass = '';
-    var fromID = '';
-    var toID = '';
-
     $(".grid").sortable({
-        connectWith: ".grid",
+        //connectWith: ".grid",
         handle: ".panel-heading",
         tolerance: 'pointer',
-        placeholder: 'col-md-6 well placeholder title',
-        forceHelperSize: true
+        items: '.panel',
+        connectWith: ".grid",
+        revert: true,
+        opacity: 0.5,
+        dropOnEmpty: true,
+        placeholder: 'panel-header placeholder panel-body',
+        forceHelperSize: true,
+        start: function(e, ui){
+            ui.placeholder.height(ui.item.height());
+            ui.placeholder.html('<div style="font-size:16pt;text-align:center;margin-top:'+((ui.item.height())/2)+'px;color:#999999;">Drag your gadget here!</div>');
+        }
+    });
+}
+
+function setGadgetColor() {
+    $('.colorize').click(function() {
+        var color = $(this).css('background-color');
+        var textColor = '#FFFFFF';
+
+        if(color == 'rgb(245, 245, 245)') {
+            textColor = '#000000';
+        }
+
+        $(this).parents(':eq(8)').css('background-color', color).css('color', textColor);
     });
 }
