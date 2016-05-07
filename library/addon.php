9b25871c149ad7ed25ec5a6a24427677a48af151
Pisti <szana8@gmail.com>
2016-05-07 21:36:44 +0200

    -

--- library/addon.php
+++ library/addon.php
@@ -4,7 +4,9 @@
  * File: Addon.php
  * Namespace: library
  *
- * Description of Addon
+ * Description of Addon: A keretrendszerhez és a moduleokhoz tartozó
+ * plugin-okat kezelő osztály. Tartalmazza az installáláshoz szükséges
+ * metódusokat és egyéb függvényeket.
  *
  *
  *  Version     Date            Author               Changelog
@@ -25,6 +27,8 @@ class Addon
 # 2. Public Properties #########################################################
 ################################################################################
 
+
+
 ################################################################################
 # 3. Protected Properties ######################################################
 ################################################################################
@@ -34,10 +38,37 @@ class Addon
 # 4. Public Methods ############################################################
 ################################################################################
 
-    public static function getAddons() {
-        $loc_DB = new \library\Mysql(\PDO::FETCH_ASSOC);
+    /**
+     * Lekérdezi az adatbázisból a plugin-ek listáját.
+     *
+     * @return \stdClass    A pluginek listáját tartalmazó objektum.
+     * @version 1.0
+     * @access publiuc
+     */
+    public static function getAddons() : array {
+        $loc_DB = new \library\Mysql(\PDO::FETCH_OBJ);
         $loc_List = $loc_DB->query('SELECT * FROM fnd_addons ORDER BY name');
         return $loc_List;
+    }
+
+    /**
+     * Megnézi, hogy a paraméterben kapott zip file-ban létezik e a pom.xml file.
+     * @param string $pin_FileName          A vizsgált zip file
+     * @return bool                         Létezik e a pom vagy sem
+     */
+    public static function isPOMExists(string $pin_FileName) : bool {
+        $obj_Zip = new Zip();
+        $obj_Zip->setZipFile($pin_FileName);
+        (array)$loc_Zip = $obj_Zip->getZipFileContent();
+        (array)$loc_Tmp = array();
+
+        foreach ($loc_Zip as $loc_File)
+           $loc_Tmp = array_merge($loc_Tmp, explode('/', $loc_File));
+
+        if(in_array('pom.xml', $loc_Tmp))
+            return true;
+
+        return false;
     }
 
 ################################################################################
--- library/zip.php
+++ library/zip.php
@@ -15,6 +15,137 @@
 namespace library;
 
 
-class Zip {
+class Zip extends File {
+
+################################################################################
+# 1. Constants #################################################################
+################################################################################
+
+    /**
+     * A hiba a file megnyitásakor
+     */
+    const   errorZipFileOpen           = "xz00000";
+
+################################################################################
+# 2. Public Properties #########################################################
+################################################################################
+
+################################################################################
+# 3. Protected Properties ######################################################
+################################################################################
+
+    /**
+     * Ebben a property-ben tároljuk a zip file nevét amivel az aktuális
+     * példányban dolgozunk.
+     * @var string
+     */
+    private $_zipFile;
+
+    /**
+     * Ebben a propeerty-ben tároljuk a zip file tartalmát.
+     * @var array
+     */
+    private $_zipStream;
+
+################################################################################
+# 4. Public Methods ############################################################
+################################################################################
+
+    /**
+     * Vissza adja a property-ben megadott zip file tartalmát.
+     * @return mixed            A zip file tartalma
+     * @version 1.0
+     * @acces public
+     */
+    public function getZipFileContent() {
+        $obj_Zip = $this->_extractZip();
+        $this->_getZipFile($obj_Zip);
+        return $this->_zipStream;
+    }
+
+    /**
+     * Vissza adja a zipFile property értékét.
+     * @return mixed            A property értéke
+     * @versiom 1.0
+     * @access public
+     */
+    public function getZipFile() {
+        return $this->_zipFile;
+    }
+
+    /**
+     * Beállítja a zipFile property értékét.
+     * @param mixed $zipFile    A prtoperty értéke
+     * @version 1.0
+     * @access public
+     */
+    public function setZipFile($zipFile) {
+        $this->_zipFile = $zipFile;
+    }
+
+    /**
+     * Vissza adja a zipstream property értékét.
+     * @return mixed            A property értéke
+     * @version 1.0
+     * @access public
+     */
+    public function getZipStream() {
+        return $this->_zipStream;
+    }
+
+    /**
+     * Beállítja a zipStream property-t.
+     * @param mixed $zipStream  A prtoperty értéke
+     * @version 1.0
+     * @access public
+     */
+    public function setZipStream($zipStream) {
+        $this->_zipStream = $zipStream;
+    }
+
+
+
+################################################################################
+# 5. Protected Methods #########################################################
+################################################################################
+
+################################################################################
+# 6. Private Methods ###########################################################
+################################################################################
+
+    /**
+     * Megnyitja a _zipFile property-ben levő zip-et.
+     * @return \ZipArchive              A megnyitott file objektuma.
+     * @version 1.0
+     * @access private
+     */
+    private function _extractZip() : \ZipArchive {
+        $obj_Zip = new \ZipArchive();
+        if(!$this->getFileExists(APPS_D_ROOT . 'tmp/' . $this->_zipFile)) {
+            $obj_Zip->status = 'error';
+            return $obj_Zip;
+        }
+        
+        try {
+            $obj_Zip->open(APPS_D_ROOT . 'tmp/' . $this->_zipFile);
+            return $obj_Zip;
+        } catch(\Exception $e) {
+            Debug::setDebugMessage(array(__METHOD__, self::errorZipFileOpen, "{MSG.ERROR.ERROR_ZIP_FILE_OPEN}", "err", $this->_zipFile));
+            $obj_Zip->status = 'error';
+            return $obj_Zip;
+        }
+    }
+
+    /**
+     * A paraméterben kapott zip objektumból kiveszi a file-ok és mappák neveit.
+     * @param \ZipArchive $pin_ZipObj   A vizsgált zip objektum.
+     * @version 1.0
+     * @access private
+     */
+    private function _getZipFile(\ZipArchive $pin_ZipObj) {
+        for($i=0;$i<$pin_ZipObj->numFiles;$i++) {
+            $this->_zipStream[] = $pin_ZipObj->getNameIndex($i);
+        }
+    }
 
 }
